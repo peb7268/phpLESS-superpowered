ccacc9ea03778edfe1d9875545a170125226f75b
peb7268 <paul@imperativedesign.net>
2012-09-10 16:55:56 -0400

    phase1 done. Now pass in php

--- init.php
+++ init.php
@@ -8,35 +8,31 @@ $less = new lessc;
 function init($file_array){
 	global $ssdir;
 	$pre_compiled_less = $ssdir.'styles.less';
-	$css = fopen($pre_compiled_less, 'a');
-	$counter = 0;
-
+	$css = fopen($pre_compiled_less, 'w');
+	
 	foreach($file_array as $file){
 		$f = fopen($file, 'a+');
-		echo 'opening file <br>';
+		$contents = fread($f, filesize($file));
 
-		while($contents = fread($f, filesize($file))){
-			
-			fwrite($css, 'pass # '.$counter.$contents);
-		}
-		$counter++;
+		fwrite($css, $contents);
+		
 		fclose($f);
 	}
 	fclose($css);
 
 	return $pre_compiled_less;
 }
-$less_files = array($ssdir.'styles1.less', $ssdir.'styles2.less');
-$input = init($less_files);
-
-
 function compileLess($less, $input, $out_path){
 	$less = $less;
 	try{
-		$less->checkedCompile($input, $out_path);
+		$less->compileFile($input, $out_path);
 	} catch( exception $e ) {
 		echo 'Error: '. $e->getMessage();
 	}
 	echo '<link rel="stylesheet" href="'.$out_path.'">';
 }
-compileLess($less, $input, 'styles.css');
+
+
+$less_files = array($ssdir.'styles1.less', $ssdir.'styles2.less');	//Tell It what to watch
+$input = init($less_files);											//Init it
+compileLess($less, $input, 'styles.css');							//Compile passing in the less object, $input from init() and the output path you want it to go to
